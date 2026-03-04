7635aa2b5f31ba9bcc81242269596fdd6aa353f3
Nikolaos Makridis <makridisnikolas@icloud.com>
2026-03-04 20:49:09 +0000

    calculations

---
+++ calculations/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / %
+
+        int friends = 10;
+
+        friends = friends / 2;
+        System.out.println(friends);
+
+        friends++;
+        System.out.println(friends);
+
+        friends--;
+        System.out.println(friends);
+
+        friends += 5;
+        System.out.println(friends);
+
+        friends = friends * 2;
+        System.out.println(friends);
+    }
+}
