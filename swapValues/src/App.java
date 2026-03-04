9ee0bbd4a4a4e1d252cf8bece4a6665eabdfaaa1
Nikolaos Makridis <makridisnikolas@icloud.com>
2026-03-04 18:09:11 +0000

    re-compiled dataTypes and helloWorld. swapValues programme created.

---
+++ swapValues/src/App.java
@@ -0,0 +1,17 @@
+import javax.print.DocFlavor.STRING;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String x = "water";
+        String y = "gin";
+
+        String temp;
+        temp = x;
+        x = y;
+
+        y = temp;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
