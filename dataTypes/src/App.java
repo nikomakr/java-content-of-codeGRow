848e6e4d95722f4cef024c402aa95d97b0e5c478
Nikolaos Makridis <makridisnikolas@icloud.com>
2026-03-04 17:35:55 +0000

    data types and printing structural content

---
+++ dataTypes/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Declaration of a variable
+        int x = 123;
+        double y = 3.14;
+        boolean z = false;
+
+        char symbol = '@';
+        String name = "codeGRow";
+
+        System.out.println("Name of the channel = "+name+symbol+x+" "+symbol);
+    }
+}
