9ea621145796edf3be14adef0f291b078e52bbcf
Nikolaos Makridis <makridisnikolas@icloud.com>
2026-03-06 20:49:52 +0000

    for while

---
+++ forWhile/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // for loop = executes a block of code a limited amount of times
+
+        for (int index = 0; index < 10; index++) {
+            System.out.println(index);
+        }
+
+        for (int j = 9; j >=0; j--) {
+            System.out.println(j);
+        }
+    }
+}
